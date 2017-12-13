6cca8978952cf12b2a30c768d918219e96e38cef
Ipshita <iroyburman89@gmail.com>
2017-12-12 17:57:06 -0800

    Initial commit

---
+++ app/src/main/java/com/ipshita/firebasedemo/firebasedemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.ipshita.firebasedemo.firebasedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("message");
+        databaseReference.setValue("Hello Firebase");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(MainActivity.this, value,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        }); 
+
+    }
+
+    /*{
+        "rules": {
+        ".read": "auth != null",
+                ".write": "auth != null"
+    }
+    }*/
+}
